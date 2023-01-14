4056ecd7190f543c82f0e411aacff926f2f5c771
MalauD <malaury.dutour@gmail.com>
2023-01-14 19:19:17 +0100

    séance labo

--- Code/Tipe/src/main.cpp
+++ Code/Tipe/src/main.cpp
@@ -10,7 +10,7 @@ void setup() {
     auto func = [](double x, std::array<double, 3> coef) {
         return coef[0] / std::pow(x, coef[1]) + coef[2];
     };
-    GradientDescSettings<double, 3> settings(0.5, 1e-8, 10000, {0.5, 0.5, 0.5});
+    GradientDescSettings<double, 3> settings(0.1, 1e-8, 10000, {0.5, 0.5, 0.5});
 
     auto factory_grad =
         std::make_unique<GradientDescFactory<double, 3>>(func, settings);
