d322ec2dd493fc1a6ecaec584d30ff21db0afd11
Malaury <malaurydutour@outlook.fr>
2023-02-26 17:58:37 +0100

    Ajout de relevés

--- Code/FileDecoder/main.c
+++ Code/FileDecoder/main.c
@@ -1,44 +1,61 @@
 #include "stdio.h"
 #include <stdlib.h>
 #include "stdint.h"
+#include "string.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     FILE *in, *out;
     uint16_t time;
     int16_t ch1, ch2, ch3, ch4;
 
-    if(argc != 2 && argc != 3) {
-        printf("Incorrect number of arguments, provided: %d, expected: 1 \n" , argc - 1);
+    if (argc != 2 && argc != 3)
+    {
+        printf("Incorrect number of arguments, provided: %d, expected: 1 \n", argc - 1);
         return -1;
     }
 
     in = fopen(argv[1], "rb");
-    out = fopen("out.csv", "w");
 
-    if(in == NULL) {
+    // Same name as input file, but with .csv extension instead of .TXT for the output
+    char *outName = malloc(strlen(argv[1]) + 5);
+    strcpy(outName, argv[1]);
+    outName[strlen(argv[1]) - 4] = '\0';
+    strcat(outName, ".csv");
+    out = fopen(outName, "w");
+
+    if (in == NULL)
+    {
         printf("Could not open file %s \n", argv[1]);
         return -1;
     }
-    
 
     int offset = 0;
-    if(argc == 3) {
+    if (argc == 3)
+    {
         offset = atoi(argv[2]);
-    } else {
+    }
+    else
+    {
         printf("No offset provided, trying to find offset in file \n");
         char c;
-        while (fread(&c, sizeof(char), 1, in) == 1) {
-            if (c == '#') {
+        while (fread(&c, sizeof(char), 1, in) == 1)
+        {
+            if (c == '#')
+            {
                 offset = ftell(in);
                 // Try to see if the next 9 characters are also #
                 int i;
-                for (i = 0; i < 9; i++) {
+                for (i = 0; i < 9; i++)
+                {
                     fread(&c, sizeof(char), 1, in);
-                    if (c != '#') {
+                    if (c != '#')
+                    {
                         break;
                     }
                 }
-                if (i == 9) {
+                if (i == 9)
+                {
                     offset += 9;
                     printf("Found offset at %d \n", offset);
                     break;
@@ -50,7 +67,8 @@ int main(int argc, char *argv[]) {
     printf("Converting %s to out.csv, offset %d \n", argv[1], offset);
     fprintf(out, "Time,Channel 1,Channel 2,Channel 3,Channel 4\n");
     fseek(in, offset, SEEK_SET);
-    while (fread(&time, sizeof(uint16_t), 1, in) == 1) {
+    while (fread(&time, sizeof(uint16_t), 1, in) == 1)
+    {
         fread(&ch1, sizeof(int16_t), 1, in);
         fread(&ch2, sizeof(int16_t), 1, in);
         fread(&ch3, sizeof(int16_t), 1, in);
---
+++ Code/Notebooks/data/README.md
@@ -0,0 +1,6 @@
+## Utilisation des donnés
+### Formattage des noms de fichier
+Exemple :
+`002_chaussure_revetement.csv`
+
+En premier on retrouve le numéro de session et ensuite c'est la chaussure et enfin le revetement
